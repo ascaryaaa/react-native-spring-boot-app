7f8574865b26d9b801e769252899b7949ee0a9aa
ascaryaaa <allaamascarya@gmail.com>
2024-03-05 21:00:38 +0700

    added server side from spring initializer

---
+++ server-side/src/test/java/com/app/serverside/ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.serverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
