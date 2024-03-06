db86a609cc0277ca6f5a6c9e6048021c38f6d780
ascaryaaa <allaamascarya@gmail.com>
2024-03-06 08:21:46 +0700

    Added product model class

---
+++ server-side/src/main/java/com/app/serverside/model/Product.java
@@ -0,0 +1,22 @@
+package com.app.serverside.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "explanation")
+    private String explanation;
+}
